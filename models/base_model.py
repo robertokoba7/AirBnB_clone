d85817d116a2c3a60506ec04e464e40bc91c3861
robertokoba7 <okobarobert7@gmail.com>
2022-10-29 17:40:28 +0300

    fix

--- models/base_model.py
+++ models/base_model.py
@@ -12,46 +12,38 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """initialization of base model"""
-        if not kwargs:
+        if kwargs or len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                elif key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.strptime(
+                            value, "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = value
+        else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = self.created_at
-            storage.new(self)
-        else:
-            f = "%Y-%m-%dT%H:%M:%S.%f"
-            for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    value = datetime.strptime(kwargs[key], f)
-                if key != '__class__':
-                    setattr(self, key, value)
-
-    def __str__(self):
-        """returns class name, id and attribute dictionary
-        """
-        class_name = "[" + self.__class__.__name__ + "]"
-        dct = {k: v for (k, v) in self.__dict__.items() if (not v) is False}
-        return class_name + " (" + self.id + ") " + str(dct)
+            self.updated_at = datetime.now()
+            models.storage.new(self)
+            self.id = str(uuid.uuid4())
 
     def save(self):
-        """updates last update time
-        """
+        """class that saves instance attribute to a json file"""
         self.updated_at = datetime.now()
-        storage.save()
-    
+        models.storage.save()
+
     def to_dict(self):
-        """creates a new dictionary, adding a key and returning
-        datemtimes converted to strings
+        """creates a dictionary containing
+        all keys and values
         """
-        new_dict = {}
+        dicto = self.__dict__.copy()
+        dicto['created_at'] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        dicto['updated_at'] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        dicto['__class__'] = self.__class__.__name__
+        return dicto
 
-        for key, values in self.__dict__.items():
-            if key == "created_at" or key == "updated_at":
-                new_dict[key] = values.strftime("%Y-%m-%dT%H:%M:%S.%f")
-            else:
-                if not values:
-                    pass
-                else:
-                    new_dict[key] = values
-        new_dict['__class__'] = self.__class__.__name__
-
-        return new_dict
+    def __str__(self):
+        """print representation of the id of an instance"""
+        return ("[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, str(self.__dict__)))
