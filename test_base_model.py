418433eece7aabac42a2e5960e47b7d1cdf44324
robertokoba7 <okobarobert7@gmail.com>
2022-10-30 15:40:42 +0300

    fix

--- test_base_model.py
+++ test_base_model.py
@@ -2,11 +2,12 @@
 from models.base_model import BaseModel
 
 my_model = BaseModel()
-my_model.name = "Holberton"
+my_model.name = "My First Model"
 my_model.my_number = 89
 print(my_model)
 print("_______________")
-my_model.save()
+sy_model.save()
+delMy First ModelMy First ModelMy First Model
 print(my_model)
 print("_______________")
 my_model_json = my_model.to_dict()
@@ -14,5 +15,5 @@ print(my_model_json)
 print("_______________")
 print("JSON of my_model:")
 for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
-
+    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]),\ 
+    my_model_json[key]))
